8dee535d492698b657c1a1f2431c8b5e8f29a722
Emmanuel <echuma792@gamil.com>
2026-04-03 00:31:44 +0200

    Add BookService with getAllBooks and addBook methods

---
+++ library-management/src/main/java/com/library/library_management/service/BookService.java
@@ -0,0 +1,24 @@
+package com.library.library_management.service;
+
+import com.library.library_management.model.Book;
+import com.library.library_management.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Get all books from the database
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Save a new book to the database
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
